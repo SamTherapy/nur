5e0c630b442a3442bd9648ea4169e5dbf7b5bc28
Sam Therapy <sam@samtherapy.net>
2023-11-08 22:19:12 +0100

    Do the template

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  awl-dns = pkgs.callPackage ./pkgs/awl {};
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
